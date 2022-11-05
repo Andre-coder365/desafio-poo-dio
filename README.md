17265f9331644bedc33706588468319635c83dad
Andre S Oliveira <andreoliveira365@hotmail.com>
2022-11-05 17:31:58 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Desafio de Projeto [DIO](https://www.dio.me/) - Programação Orientada a Objeto - POO 
+
+Abstração de um Bootcamp com suas características tais como nome do curso, descrição do curso, carga horária, Xp's (pontuação) e outros componentes, como Devs, mentoria.
+
+Para esse desafio foi feito o uso de orientação a objeto, incluindo herança e polimorfismo.
---
+++ src/br/com/dio/desafio/dominio/ZnotesDesafioPOO.java
@@ -0,0 +1,25 @@
+package br.com.dio.desafio.dominio;
+
+public class ZnotesDesafioPOO {
+    /*
+    Modificadores de acesso = public, private, protected
+    usados antes dos atributos (variáveis, constantes)
+
+    public = qualquer classe que precisar usar e modificar esses atributos vai ter acesso.
+    private = só essa classe onde ele está terá acesso diretamente
+    a esse atributo.
+    protected = só essa classe e as classes - filhas terão acesso
+    a esse atributo
+
+    Por default, se não colocar nada na frente (antes do atributo) fica public
+
+    É uma boa prática utilizar o modificador de acesso private.
+
+    Para que eu possa dizer que uma classe é filha da outra, eu uso a palavrinha
+    reservada "extends" (Ex.: public class Mentoria extends Conteudo {})
+
+
+     */
+
+
+}
